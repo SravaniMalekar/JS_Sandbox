b39a78f78ff488a455a5d31fdfb91e1d33d6a9f0
Sravani Malekar <60072148+SravaniMalekar@users.noreply.github.com>
2021-02-11 14:05:11 +0530

    Add files via upload

---
+++ 2.Variables/app.js
@@ -0,0 +1,61 @@
+//var, let, const
+
+var namePerson = "Dwight Schrute";
+console.log(namePerson);
+namePerson = "Michael";
+console.log(namePerson);
+
+//initializing var
+
+var greeting;
+console.log(greeting);
+greeting = "hello";
+console.log(greeting);
+
+//var - cannot start with number
+// letters number $ _
+
+var firstName = "John";
+var first_name = "Pam";
+var FirstName = "kevin";
+
+
+//LET
+
+let Person = "Dwight Schrute";
+console.log(Person);
+Person = "Michael";
+console.log(Person);
+
+
+//const
+
+const name = "Jim";
+console.log(name);
+//cannot reassign
+//have to assign a value 
+// const greeting;
+
+const person = {
+    name: 'john',
+    age : 20
+}
+
+person.name = 'sherlock';
+person.age = 35;
+
+console.log(person);
+
+const numbers = [1,2,3,4,5];
+
+numbers.push(6);
+
+console.log(numbers);
+
+
+
+
+
+
+
+
---
+++ 3. Data Types/app.js
@@ -0,0 +1,39 @@
+//PRIMITIVE
+
+//string
+const name = 'John Doe';
+//number
+const number = 45;
+//boolean
+const isPresent = true;
+//Null
+const car = null;
+//undefined
+let fruit;
+//symbol
+const sym = Symbol();
+
+
+//REFERENCE TYPES
+
+//Array
+const hobbies = ['baking','painting'];
+
+//Object literal
+const address = {
+    city: 'New Delhi',
+    state: 'Delhi'
+}
+
+const today = new Date();
+
+console.log(typeof name);
+console.log(typeof number);
+console.log(typeof isPresent);
+console.log(typeof car);
+console.log(typeof fruit);
+console.log(typeof sym);
+console.log(typeof hobbies);
+console.log(typeof address);
+console.log(today);
+console.log(typeof today);
